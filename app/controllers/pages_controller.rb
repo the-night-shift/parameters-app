b4823e8e96f8f13eeeb8e86186f1ad0a51b3c329
Brian <riceb53@gmail.com>
2018-03-07 20:55:38 -0600

    make games

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,34 @@
 class PagesController < ApplicationController
   def parameters_method
-    the_name = params["name"].upcase
-    location = params["location"]
-    render json: {name: the_name, location: "You are in #{location}"}
+    # get the data
+    message = params["name"].upcase
+    if message[0] == "A"
+      message = "Hey, your name starts with the first letter of the alphabet!"
+    end
+    render json: {message: message}
   end
+
+  def guess_a_number
+    # make an answer
+    answer = 36
+    # user guesses
+    user_guess = params["guess"].to_i
+    # i compare that guess to the answer
+    # p "*" * 50
+    # p 'answer'
+    # p answer.class
+    # p 'user_guess'
+    # p user_guess.class
+    # p "*" * 50
+    if answer == user_guess
+      the_message = "yay"
+    elsif answer > user_guess
+      the_message = "you guessed too low, you should guess higher"
+    elsif answer < user_guess
+      the_message = "you guessed too high, you should guess lower"
+    end
+    # if they get it right, tell them 'yay' if they get it wrong tell them they suck
+    render json: {message: the_message}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/url-params' => 'pages#parameters_method'
+  get '/number-game' => 'pages#guess_a_number'
 end
